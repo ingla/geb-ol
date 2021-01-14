b03e18fafc7336a47fa4fce3131460562cb27025
Ingvild Larsen <laingla@online.no>
2021-01-14 16:03:07 +0100

    First commit

---
+++ geb-ol/src/main/java/gebol/gebol/GebOlApplication.java
@@ -0,0 +1,13 @@
+package gebol.gebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GebOlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GebOlApplication.class, args);
+	}
+
+}
