5b6e2a3a9b0b2eaa703080074ef7ec4aea538d2d
Ingvild Larsen <laingla@online.no>
2021-01-23 18:29:19 +0100

    Fix folder structure

---
+++ src/main/java/com/example/gebol/GebOlApplication.java
@@ -0,0 +1,13 @@
+package com.example.gebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GebOlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GebOlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gebol/GebOlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GebOlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
