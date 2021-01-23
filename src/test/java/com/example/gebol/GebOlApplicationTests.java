06428afb214c5eb1eb6bebb64319b598e1b33a04
Ingvild Larsen <laingla@online.no>
2021-01-23 21:07:49 +0100

    Add simple homepage

--- src/test/java/com/example/gebol/GebOlApplicationTests.java
+++ src/test/java/com/example/gebol/GebOlApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.gebol;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GebOlApplicationTests {
+public class GebOlApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
