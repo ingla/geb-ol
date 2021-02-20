9768f35692d7d0357418ef2464bed7285ee3dfac
Ingvild Larsen <laingla@online.no>
2021-02-20 23:58:26 +0100

    Add DB support for Discipline object

--- src/main/java/com/example/gebol/controller/DisciplineController.java
+++ src/main/java/com/example/gebol/controller/DisciplineController.java
@@ -1,32 +1,49 @@
 package com.example.gebol.controller;
 
+import com.example.gebol.data.DisciplineRepository;
 import com.example.gebol.model.Discipline;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @Slf4j
 @Controller
-@RequestMapping("/discipline")
+@RequestMapping("/add-discipline")
 public class DisciplineController {
+
+    private DisciplineRepository disciplineRepository;
+
+    @Autowired
+    public DisciplineController(DisciplineRepository disciplineRepository) {
+        this.disciplineRepository = disciplineRepository;
+    }
 
     @GetMapping
     public String addDiscipline(Model model) {
         log.info("addDiscipline");
-        model.addAttribute("disciplineObject", new Discipline());
+        model.addAttribute("discipline", new Discipline());
         return "add-discipline";
     }
 
     @PostMapping
-    public String processDiscipline(@ModelAttribute Discipline discipline) {
+    public String processDiscipline(@Valid @ModelAttribute Discipline discipline, Errors errors) {
         log.info("processDiscipline in controller: new discpline " +
                 discipline.toString() + " was added");
+
+        if (errors.hasErrors()) {
+            return "add-discipline";
+        }
+
+        disciplineRepository.save(discipline);
         return "redirect:/";
     }
 }
---
+++ src/main/java/com/example/gebol/data/JdbcDisciplineRepository.java
@@ -0,0 +1,74 @@
+package com.example.gebol.data;
+
+import com.example.gebol.model.Discipline;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Repository
+@Slf4j
+public class JdbcDisciplineRepository implements DisciplineRepository {
+
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    public JdbcDisciplineRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public Iterable<Discipline> findAll() {
+        log.info("findAll");
+        return jdbc.query(
+                "select * from Discipline",
+                this::mapRowToDiscipline);
+    }
+
+    public Discipline findOne(String id) {
+        // TODO
+        return new Discipline();
+    }
+
+    public Discipline save(Discipline discipline) {
+        log.info("save");
+        PreparedStatementCreator creator =
+                new PreparedStatementCreatorFactory(
+                        "insert into Discipline (name, location, date, isCup) values (?, ?, ?, ?)",
+                        Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP, Types.BOOLEAN
+                ).newPreparedStatementCreator(
+                        Arrays.asList(
+                                discipline.getName(),
+                                discipline.getLocation(),
+                                Timestamp.valueOf(discipline.getDate()),
+                                discipline.isCup()));
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(creator, keyHolder);
+
+        return discipline;
+    }
+
+    private Discipline mapRowToDiscipline(ResultSet rs, int rowNum) throws SQLException {
+        Discipline d = new Discipline();
+        d.setId(rs.getString("id"));
+        d.setName(rs.getString("name"));
+        d.setLocation(rs.getString("location"));
+
+        Timestamp timestamp = rs.getTimestamp("date");
+        LocalDateTime locDate = timestamp.toLocalDateTime();
+        d.setDate(locDate);
+
+        d.setCup(rs.getBoolean("isCup"));
+
+        return d;
+    }
+}
