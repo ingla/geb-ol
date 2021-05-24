edba01efa313bc09078123947fdcbde6e7c16e2f
Ingvild Larsen <laingla@online.no>
2021-05-24 13:52:39 +0200

    Add creation and storage of result

---
+++ src/main/java/com/example/gebol/controller/AddResultController.java
@@ -0,0 +1,73 @@
+package com.example.gebol.controller;
+
+import com.example.gebol.data.DisciplineRepository;
+import com.example.gebol.data.ParticipantRepository;
+import com.example.gebol.data.ResultRepository;
+import com.example.gebol.model.AddResultUserInput;
+import com.example.gebol.model.Discipline;
+import com.example.gebol.model.Participant;
+import com.example.gebol.model.Result;
+import com.example.gebol.model.ResultListCreation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Controller
+public class AddResultController {
+
+    private final ResultRepository resultRepository;
+    private final ParticipantRepository participantRepository;
+    private final DisciplineRepository disciplineRepository;
+
+    @Autowired
+    public AddResultController(ResultRepository resultRepository,
+                               ParticipantRepository participantRepository,
+                               DisciplineRepository disciplineRepository) {
+        this.resultRepository = resultRepository;
+        this.participantRepository = participantRepository;
+        this.disciplineRepository = disciplineRepository;
+    }
+
+    @GetMapping("/init-add-result")
+    public String addResult(Model model) {
+        log.info("addResult");
+        Iterable<Discipline> allDisciplines = disciplineRepository.findAll();
+        model.addAttribute("allDisciplines", allDisciplines);
+        model.addAttribute("addResultUserInput", new AddResultUserInput());
+        return "init-add-result";
+    }
+
+    @PostMapping("/init-add-result")
+    public String processAddResult(Model model, AddResultUserInput addResultUserInput) {
+        log.info("processAddResult:" + addResultUserInput);
+        Iterable<Participant> allParticipants = participantRepository.findAll();
+        model.addAttribute("allParticipants", allParticipants);
+
+        ResultListCreation resultList = new ResultListCreation();
+        for (int i = 0; i < addResultUserInput.getParticipantCount(); i++) {
+            Result r = new Result();
+            r.setPlace(i + 1);
+            r.setDisciplineId(addResultUserInput.getDisciplineId());
+            resultList.addResult(r);
+        }
+
+        model.addAttribute("resultListCreation", resultList);
+        return "add-results-for-discipline";
+    }
+
+    @PostMapping("/save-results")
+    public String processAddResult(ResultListCreation resultListCreation) {
+        log.info("save-results");
+        log.info(resultListCreation.getResults().toString());
+        resultRepository.saveAll(resultListCreation.getResults());
+        return "home";
+    }
+}
---
+++ src/main/java/com/example/gebol/data/JdbcResultRepository.java
@@ -0,0 +1,49 @@
+package com.example.gebol.data;
+
+import com.example.gebol.model.Discipline;
+import com.example.gebol.model.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+@Slf4j
+public class JdbcResultRepository implements ResultRepository {
+
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    public JdbcResultRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    @Override
+    public Result save(Result result) {
+        jdbc.update("insert into Result (disciplineId, participantId, result) values (?, ?, ?)",
+                result.getDisciplineId(), result.getParticipantId(), result.getPlace());
+        return result;
+    }
+
+    @Override
+    public List<Result> saveAll(List<Result> results) {
+        for (Result result : results) {
+            save(result);
+        }
+        return results;
+    }
+}
