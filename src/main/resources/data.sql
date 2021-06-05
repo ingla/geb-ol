b3b083b92a23626447aa87626f962e50885f8dca
Sigurd Kittilsen <sigurki@ifi.uio.no>
2021-06-05 19:49:23 +0200

    medals and standings

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ insert into Discipline (id, name, location, date, isCup)
 values (1, 'Bowling', 'Bowling 1 Elverum', '2008-01-01 12:00:00+01:00', false);
 
 insert into Participant (id, name)
-values (1, 'Sigurd'),
+values (1, 'Sigurd'), (3, 'Andreas'), (4, 'Trine'), (5, 'Drugdealer'),
 (2, 'Ingvild');
 
 insert into Result (disciplineId, participantId, place)
-values (1, 1, 2),
+values (1, 1, 2), (1, 3, 3), (1, 4, 4), (1, 5, 5),
 (1, 2, 1);
